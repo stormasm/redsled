d3b25b75dd5c665151e0d5e3ce2f3881512c131c
Michael Angerman <stormasm@gmail.com>
2019-12-05 12:38:03 -0500

    change String to u32

--- examples/redis-to-file.rs
+++ examples/redis-to-file.rs
@@ -1,7 +1,7 @@
 use r2d2_redis::{r2d2, RedisConnectionManager};
 use redis::{Commands, RedisResult};
 
-fn get_hashmap_keys(key: String) -> RedisResult<Vec<String>> {
+fn get_hashmap_keys(key: String) -> RedisResult<Vec<u32>> {
     let manager = RedisConnectionManager::new("redis://localhost").unwrap();
     let pool = r2d2::Pool::builder().build(manager).unwrap();
 
