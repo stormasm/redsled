cc12efd752b2a00e98682c3990261959e53fd5da
Michael Angerman <stormasm@gmail.com>
2019-12-05 20:17:02 -0500

    grabbing sorted key and value which is json from the redis hashmap

--- examples/redis-to-file.rs
+++ examples/redis-to-file.rs
@@ -1,6 +1,8 @@
 // Note on vector sorting...
 // https://rust-lang-nursery.github.io/rust-cookbook/algorithms/sorting.html
 
+// https://doc.rust-lang.org/std/io/struct.LineWriter.html
+
 use r2d2_redis::{r2d2, RedisConnectionManager};
 use redis::{Commands, RedisResult};
 
@@ -17,8 +19,17 @@ fn main() {
     let mut keys = get_hashmap_keys("hn-story-19".to_string()).unwrap();
     keys.sort();
 
+    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let pool = r2d2::Pool::builder().build(manager).unwrap();
+
+    let pool = pool.clone();
+    let mut con = pool.get().unwrap();
+
     for key in &keys {
+        let value: RedisResult<String> = con.hget("hn-story-19".to_string(), key.to_string());
+        let json = value.unwrap();
         println!("{}", key);
+        println!("{}", json);
     }
 
     println!("Number of keys = {}", keys.len());
